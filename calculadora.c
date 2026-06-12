2566955e48abceacdb49a5829fa0ba26ce0f014d
Alejandro Méndez <business.sorii@gmail.com>
2026-06-11 18:32:39 -0600

    Se agrega código base de calculadora

---
+++ calculadora.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void suma();
+void resta();
+void multiplicacion();
+void division();
+
+int main() {
+    int opcion;
+
+    printf("Calculadora sencilla\n");
+    printf("1. Suma\n");
+    printf("2. Resta\n");
+    printf("3. Multiplicacion\n");
+    printf("4. Division\n");
+    printf("Elige una opcion: ");
+    scanf("%d", &opcion);
+
+    switch(opcion) {
+        case 1:
+            suma();
+            break;
+        case 2:
+            resta();
+            break;
+        case 3:
+            multiplicacion();
+            break;
+        case 4:
+            division();
+            break;
+        default:
+            printf("Opcion no valida\n");
+    }
+
+    return 0;
+}
+
+void suma() {
+    printf("Operacion suma pendiente de desarrollar.\n");
+}
+
+void resta() {
+    printf("Operacion resta pendiente de desarrollar.\n");
+}
+
+void multiplicacion() {
+    printf("Operacion multiplicacion pendiente de desarrollar.\n");
+}
+
+void division() {
+    printf("Operacion division pendiente de desarrollar.\n");
+}
